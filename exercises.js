a52ce1bd3225f46359179a542b2e3e3e81af07a4
unknown <fr.heggeness@gmail.com>
2019-02-11 19:30:06 -1000

    q2

--- exercises.js
+++ exercises.js
@@ -18,7 +18,11 @@ while (i<10){
 
 /*1. Use a while loop and print out a list of numbers from 1 to 10*/
 
-
+var i = 1;
+while(i <= 10){
+	console.log(i);
+	i++
+}
 
 /*2. Use a while loop to create the following message:
 
@@ -28,8 +32,11 @@ while (i<10){
 ...
 ...
 "Now serving 11"*/
-
-
+var i = 1;
+while (i <= 11){
+	console.log('Now serving ' + i +'.');
+	i++
+}
 
 /*3. Use a while loop to loop through the tvShows array and print out the following:
 
